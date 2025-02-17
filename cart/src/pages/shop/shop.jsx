2ed83b62d54bc81d869f0af313a47979508f502f
Suvadeep <suvadeepm0709@gmail.com>
2025-02-17 18:23:52 +0530

    added responsiveness to pages

--- cart/src/pages/shop/shop.jsx
+++ cart/src/pages/shop/shop.jsx
@@ -1,25 +1,3 @@
-/* This code is from frontend final working 
-// import React from "react";
-// import { PRODUCTS } from "../../products";
-// import { Product } from "./product";
-// import "./shop.css";
-
-// export const Shop = () => {
-//   return (
-//     <div className="shop">
-//       <div className="shopTitle">
-//         <h1>Shop</h1>
-//       </div>
-//       <div className="products">
-//         {PRODUCTS.map((product) => (
-//           <Product data={product} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-*/
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Product } from "./product";
